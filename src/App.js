4e23583bad49184e5480679939a262187ae23b2a
Shubham-ar <shubham.jhamb2000@gmail.com>
2021-05-27 17:44:24 +0530

    commit on master

--- src/App.js
+++ src/App.js
@@ -1,19 +1,23 @@
 import React from 'react'
-import {useReducer} from 'react'
+import { useReducer } from 'react'
 // we are not using useContext this time
-import {Container} from 'reactstrap'
+import { Container } from 'reactstrap'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import './App.css'
 
-import {TodoContext} from './context/TodoContext'
+import { TodoContext } from './context/TodoContext'
 import todoReducer from './context/reducer'
+import TodoForm from './components/TodoForm'
+//uuid is used to install UUID which is a tool used to generate unique ids
 
-const App=()=>{
-  const [todos,dispatch]=useReducer(todoReducer,[]);
-  return(
-    <TodoContext.Provider value={{todos,dispatch}}>
+
+const App = () => {
+  const [todos, dispatch] = useReducer(todoReducer, []);
+  return (
+    <TodoContext.Provider value={{ todos, dispatch }}>
       <Container fluid>
         <h1>TodoApp with Context API</h1>
+        <TodoForm />
       </Container>
 
     </TodoContext.Provider>
